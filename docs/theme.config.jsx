6692c8a81cd4951dc2c1ccfad0ffbb3908c04f40
Adrian Schubek <adrian.schubek@gmail.com>
2023-12-12 22:37:54 +0100

    docs: init

---
+++ docs/theme.config.jsx
@@ -0,0 +1,27 @@
+export default {
+  logo: (
+    <span>
+      react-native-paper-<span style={{ fontWeight: 700 }}>fastalerts</span>
+    </span>
+  ),
+  project: {
+    link: "https://github.com/adrianschubek/react-native-paper-fastalerts",
+  },
+  docsRepositoryBase:
+    "https://github.com/adrianschubek/react-native-paper-fastalerts/tree/main/docs",
+  // primaryHue: 190,
+  // primarySaturation: { dark: 31, light: 48 },
+  toc: {
+    backToTop: true,
+  },
+  footer: {
+    text: (
+      <span>
+        © {new Date().getFullYear()} {' '}
+        <a href="https://adrianschubek.de" target="_blank">
+          Adrian Schubek
+        </a>
+      </span>
+    )
+  }
+};
